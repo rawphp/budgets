8f17649943d6de1392d864870ec1f3de813b8d93
Tom Kaczocha <tom.kaczocha@originalsolutions.com.au>
2024-03-23 07:07:32 +1000

    clear snapshot

--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -10,8 +10,6 @@ test('profile page is displayed', function () {
 
     $response = $this->get('/profile');
 
-    expect($response)->toMatchSnapshot();
-
     $response
         ->assertOk()
         ->assertSeeVolt('profile.update-profile-information-form')
